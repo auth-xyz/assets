d16ba0378f86be2d7bd3e406bc20342beaffdda9
auth-xyz <authenticover@gmail.com>
2024-10-16 03:31:27 -0400

    initial

---
+++ templates/package.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    # Write packages here
+  ];
+
+  home.programs = [
+    # Write programs here with the layout:
+    # program = {
+    # enable = true;
+    #};
+  ];
+}
